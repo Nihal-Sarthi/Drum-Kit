dfe048a6e43c6aaec9f884eccb7038cf0fdb9b99
Nihal-Sarthi <87559764+Nihal-Sarthi@users.noreply.github.com>
2024-06-18 05:27:05 +0000

    Changes committed: 	modified:   index.html 	modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-document.querySelector("button");
+// To check which button with drum class is being clicked
 
-function handleClick(){
-    alert("button is clicked!");
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+// Play sound by mouse click
+
+function handleClick() {
+  let buttonClicked = this.innerHTML;
+  handleSound(buttonClicked);
+}
+
+// Play sound by using keyboard button press
+
+document.addEventListener("keydown", (e) => {
+  let keyPressed = e.key;
+  handleSound(keyPressed);
+});
+
+// A function to check the values and play sound accordingly
+
+function handleSound(instrument) {
+  switch (instrument) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "j":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "k":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "l":
+      var kickbass = new Audio("sounds/kick-bass.mp3");
+      kickbass.play();
+      break;
+    default:
+      console.log(keyPressed);
+  }
 }
